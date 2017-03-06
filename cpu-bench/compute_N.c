3803c9357384ed469cca81c8414e9cfada75cdab
Werner Fouche <werner.fouche@aciworldwide.com>
2017-03-06 13:15:47 +0200

    Added machine code example.

--- cpu-bench/compute_N.c
+++ cpu-bench/compute_N.c
@@ -42,6 +42,13 @@ int main(int argc, char* argv[])
     gettimeofday(&start, NULL);
     {
         while (n) { n -= 1; }
+		//
+		// Intel x64 (gcc -O) - machine code.
+		//
+        // .L9:
+        //       subq    $1, %rbx
+        //       jne     .L9
+		//
     }
     gettimeofday(&end, NULL);
 
