8bdbaf6fe702d0d3373589b4e4ae819c00c69d95
Werner Fouche <werner.fouche@gmail.com>
2017-05-15 21:49:01 +0100

    CPU-bench: created three run scripts for Windows.

--- cpu-bench/run-windows-1.cmd
+++ cpu-bench/run-windows-1.cmd
@@ -1,3 +1,3 @@
 REM python cpu-bench.py --auto --csv_report --si=00:05:00 --sc=3
 
-python cpu-bench.py --auto 
+C:\Python35\python.exe cpu-bench.py --auto 
---
+++ cpu-bench/run-windows-2.cmd
@@ -0,0 +1,3 @@
+REM python cpu-bench.py --auto --csv_report --si=00:05:00 --sc=3
+
+C:\Python35\python.exe cpu-bench.py --auto --csv_report  --si=00:05:00  --sc=12
