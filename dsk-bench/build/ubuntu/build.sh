81b663656f818bdd08d7d09af9c64ef9cb8616b0
Werner Fouche <werner.fouche@gmail.com>
2017-05-17 20:32:17 +0200

    DSK-bench: fio compiled for Ubuntu 17.04 (AMD64)

--- dsk-bench/build/ubuntu/build.sh
+++ dsk-bench/build/ubuntu/build.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
+
+export FIO=fio-2.19
+
 sudo apt-get install build-essential
 sudo apt-get install zlib1g-dev
 sudo apt-get install libaio1
 sudo apt-get install libaio-dev
 
-wget https://github.com/axboe/fio/archive/fio-2.6.tar.gz
-tar xfvz fio-2.6.tar.gz
-cd fio-fio-2.6
+wget https://github.com/axboe/fio/archive/$FIO.tar.gz
+tar xfvz $FIO.tar.gz
+cd fio-$FIO
 ./configure
 make
 sudo make install
+
 cd ..
-rm -f -r fio-fio-2.6
-rm -f    fio-2.6.tar.gz
+rm -f -r fio-$FIO
+rm -f    $FIO.tar.gz
