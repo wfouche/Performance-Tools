6e1f39d40807937710d063fd0759a72b1af38b65
Werner Fouché <werner.fouche@gmail.com>
2019-02-11 08:51:40 +0200

    Install 64bit Python 3.6.8

--- cpu-bench/windows/scripts/py_install_1.cmd
+++ cpu-bench/windows/scripts/py_install_1.cmd
@@ -1,3 +1,3 @@
 set PY_HOME=C:\Python36
 
-python-3.6.8.exe /quiet /passive InstallAllUsers=1 PrependPath=0 TargetDir=%PY_HOME%
+python-3.6.8-amd64.exe /quiet /passive InstallAllUsers=1 PrependPath=0 TargetDir=%PY_HOME%
