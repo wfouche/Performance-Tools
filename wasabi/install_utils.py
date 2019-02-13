86353ec606bceb0637255178f5e8702d92c084ed
wfouche <werner.fouche@aciworldwide.com>
2019-02-13 08:31:24 +0200

    Removed code for the generic installer.

--- wasabi/install_utils.py
+++
@@ -1,8 +0,0 @@
-
-def get_elapsed_time(t_begin, t_end):
-    x = int(t_end - t_begin)
-    hh = (x / 3600)
-    mm = ((x - (hh*3600)) / 60)
-    ss = (x - (hh*3600) - (mm*60))
-    return "%02d:%02d:%02d"%(hh,mm,ss)
-
