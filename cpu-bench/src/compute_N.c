7b1ebadc7cc6de95154365219dc337d738c5a162
wfouche <werner.fouche@gmail.com>
2019-06-25 19:28:58 +0200

    Restructured the code to allow future support for multiple threads.

--- cpu-bench/src/compute_N.c
+++ cpu-bench/src/compute_N.c
@@ -6,56 +6,40 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <pthread.h>
+#include <errno.h>
+#include <ctype.h>
+
 /* ----------------------------------------------------------------------- */
 
-int main(int argc, char* argv[])
-{
+void  compute_N(int waiting, long n) {
     struct timeval start, end;
     long secs_used,micros_used;
-
-    if (argc == 1)
-    {
-	printf ("0.000\n");
-        return 0;
-    }
-
-
     time_t        time_utc;
-    struct tm *   time_now;
+    struct tm *   time_now;	
 
-    int waiting = 0;
-    if (argc > 2)
-    {
-        waiting = atoi(argv[2]);
-    }
-    while (waiting)
-    {
+    while (waiting) {
         time(&time_utc);
         time_now = localtime(&time_utc);
-        if (time_now->tm_sec == 0)
-        {
+        if (time_now->tm_sec == 0) {
             waiting = 0;
-        }
-        else
-        {
+        } else {
             // https://stackoverflow.com/questions/1157209/is-there-an-alternative-sleep-function-in-c-to-milliseconds
-	    // sleep for 10 milliseconds
+            // sleep for 10 milliseconds
             usleep(10*1000);
         }
     }
 
-    long n = atol(argv[1]);
-    gettimeofday(&start, NULL);
+    gettimeofday(&start, NULL); 
     {
-
         while (n) { n -= 1; }
-	//
-	// Intel x64 (gcc -O -S) - machine code.
-	//
-	// .L9:
-	//       subq    $1, %rbx
-	//       jne     .L9
-	//
+		//
+		// Intel x64 (gcc -O -S) - machine code.
+		//
+		// .L9:
+		//       subq    $1, %rbx
+		//       jne     .L9
+		//
     }
     gettimeofday(&end, NULL);
 
@@ -63,8 +47,36 @@ int main(int argc, char* argv[])
     micros_used= ((secs_used*1000000) + end.tv_usec) - (start.tv_usec);
 
     double m = micros_used / 1000000.0;
-    printf("%lf\n", m);
+    printf("%lf\n", m);	
+}
 
+/* ----------------------------------------------------------------------- */
+
+int main(int argc, char* argv[]) {
+
+    if (argc == 1) {
+        printf ("0.000\n");
+        return 0;
+    }
+
+    long n = atol(argv[1]);
+
+    int waiting = 0;
+	int num_threads = 0;
+    if (argc == 3) {
+		waiting = 1;
+        num_threads = atoi(argv[2]);
+    }
+ 	
+    if (num_threads == 0) {
+        // Multiple processes
+        compute_N(waiting, n);
+    } else {
+        // Multiple threads
+        printf("multi-threading: not yet support.\n");
+        exit(1);
+    }
+	
     return 0;
 }
 
