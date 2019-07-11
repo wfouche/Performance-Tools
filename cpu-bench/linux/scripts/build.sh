61d3095050086d40098566ff6f7f7547925d3d2d
Werner Fouche <werner.fouche@gmail.com>
2019-07-11 16:51:14 +0200

    Linux - created and tested a multi-threaded compute_N program.

--- cpu-bench/linux/scripts/build.sh
+++ cpu-bench/linux/scripts/build.sh
@@ -6,7 +6,7 @@ sudo docker run -t -i --rm \
   -v `pwd`:/io \
   phusion/holy-build-box-64:latest \
   /hbb_exe/activate-exec \
-  bash -x -c 'gcc -O -fvisibility=hidden -I/hbb_exe/include /io/compute_N.c -o /io/compute_N $LDFLAGS'
+  bash -x -c 'gcc -O -fvisibility=hidden -I/hbb_exe/include /io/compute_N.c -o /io/compute_N -lpthread $LDFLAGS'
 
 rm compute_N.c
 mv -f compute_N ..
--- cpu-bench/src/compute_N.c
+++ cpu-bench/src/compute_N.c
@@ -1,88 +1,129 @@
 /* ----------------------------------------------------------------------- */
 
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <sys/time.h>
+
 #include <time.h>
+
 #include <unistd.h>
 
 #include <pthread.h>
+
 #include <errno.h>
+
 #include <ctype.h>
 
 /* ----------------------------------------------------------------------- */
 
-double compute_N(int waiting, long n) {
-    struct timeval start, end;
-    long secs_used,micros_used;
-    time_t        time_utc;
-    struct tm *   time_now;	
+long N = 0;
 
-    while (waiting) {
-        time(&time_utc);
-        time_now = localtime(&time_utc);
-        if (time_now->tm_sec == 0) {
-            waiting = 0;
-        } else {
-            // https://stackoverflow.com/questions/1157209/is-there-an-alternative-sleep-function-in-c-to-milliseconds
-            // sleep for 10 milliseconds
-            usleep(10*1000);
-        }
+/* ----------------------------------------------------------------------- */
+
+double compute_N(int waiting) {
+  struct timeval start, end;
+  long secs_used, micros_used;
+  time_t time_utc;
+  struct tm * time_now;
+
+  long n = N;
+
+  while (waiting) {
+    time( & time_utc);
+    time_now = localtime( & time_utc);
+    if (time_now->tm_sec == 0) {
+      waiting = 0;
+    } else {
+      // https://stackoverflow.com/questions/1157209/is-there-an-alternative-sleep-function-in-c-to-milliseconds
+      // sleep for 10 milliseconds
+      usleep(10 * 1000);
     }
+  }
 
-    gettimeofday(&start, NULL); 
-    {
-        while (n)
-        { 
-            n -= 1;
-            //
-            // Intel x64 (gcc -O -S) - machine code.
-            //
-            // .L9:
-            //       subq    $1, %rbx
-            //       jne     .L9
-            //
-	}
+  gettimeofday( & start, NULL); {
+    while (n) {
+      n -= 1;
+      //
+      // Intel x64 (gcc -O -S) - machine code.
+      //
+      // .L9:
+      //       subq    $1, %rbx
+      //       jne     .L9
+      //
     }
-    gettimeofday(&end, NULL);
+  }
+  gettimeofday( & end, NULL);
 
-    secs_used =(end.tv_sec - start.tv_sec);
-    micros_used = ((secs_used*1000000) + end.tv_usec) - (start.tv_usec);
+  secs_used = (end.tv_sec - start.tv_sec);
+  micros_used = ((secs_used * 1000000) + end.tv_usec) - (start.tv_usec);
 
-    double m = micros_used / 1000000.0;
-    // printf("%lf\n", m);	
-    return m;
+  double m = micros_used / 1000000.0;
+  // printf("%lf\n", m);	
+  return m;
 }
 
 /* ----------------------------------------------------------------------- */
 
-int main(int argc, char* argv[]) {
+void * worker_thread(void * x_void_ptr) {
+  int waiting = 1;
+  double elapsed_time = compute_N(waiting);
 
-    if (argc == 1) {
-        printf ("0.000\n");
-        return 0;
-    }
+  double * x_ptr = (double * ) x_void_ptr;
+  *x_ptr = elapsed_time;
 
-    long n = atol(argv[1]);
+  return NULL;
+}
 
-    int waiting = 0;
-	int num_threads = 0;
-    if (argc == 3) {
-        waiting = 1;
-        num_threads = atoi(argv[2]);
-    }
- 	
-    if (num_threads == 0) {
-        // Multiple processes
-        double elapsed_time = compute_N(waiting, n);
-        printf("%lf\n", elapsed_time);	
-    } else {
-        // Multiple threads
-        printf("compute_N.c: multi-threading - not yet support.\n");
-        exit(1);
-    }
-	
+/* ----------------------------------------------------------------------- */
+
+int main(int argc, char * argv[]) {
+
+  if (argc == 1) {
+    printf("0.000\n");
     return 0;
+  }
+
+  long n = atol(argv[1]);
+
+  N = n;
+
+  int waiting = 0;
+  int num_threads = 0;
+  if (argc == 3) {
+    waiting = 1;
+    num_threads = atoi(argv[2]);
+  }
+
+  if (num_threads == 0) {
+    // Multiple processes
+    double elapsed_time = compute_N(waiting);
+    printf("%lf\n", elapsed_time);
+  } else {
+    // Multiple threads
+    double elapsed_times[num_threads];
+    pthread_t threads[num_threads];
+
+    for (int i = 0; i < num_threads; i++) {
+      elapsed_times[i] = -1.0;
+
+      if (pthread_create( &threads[i], NULL, worker_thread, &elapsed_times[i])) {
+        fprintf(stderr, "Error creating thread\n");
+        return 1;
+      }
+    }
+
+    for (int i = 0; i < num_threads; i++) {
+      if (pthread_join(threads[i], NULL)) {
+        fprintf(stderr, "Error joining thread\n");
+        return 2;
+      }
+      printf("%lf\n", elapsed_times[i]);
+    }
+  }
+
+  return 0;
 }
 
 /* ----------------------------------------------------------------------- */
