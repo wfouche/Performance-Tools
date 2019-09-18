437d87cabab6346709646f522ffb86bf92022c80
uppdev <uppdev@cpf-bench-vm.am.tsacorp.com>
2019-09-18 09:52:05 -0400

    Fixed path to config folder.

--- dsk-bench/scripts/lnx/test_data_read.sh
+++ dsk-bench/scripts/lnx/test_data_read.sh
@@ -3,5 +3,5 @@ read -p "Press [Enter] key to start disk benchmark..."
 
 export FIO_FILENAME=/tmp/fio.dat
 
-$FIO --ioengine=libaio ../config/random_read_8k_pages_10gb_file_size.fio
+$FIO --ioengine=libaio ../../config/random_read_8k_pages_10gb_file_size.fio
 
--- dsk-bench/scripts/lnx/test_data_write.sh
+++ dsk-bench/scripts/lnx/test_data_write.sh
@@ -3,4 +3,4 @@ read -p "Press [Enter] key to start disk benchmark..."
 
 export FIO_FILENAME=/tmp/fio.dat
 
-$FIO --ioengine=libaio ../config/random_write_8k_pages_10gb_file_size.fio
+$FIO --ioengine=libaio ../../config/random_write_8k_pages_10gb_file_size.fio
